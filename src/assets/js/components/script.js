7c6d6c8d114475fdac8282ad3390871c9fec6065
VladRadzivil <vladradziviloff@gmail.com>
2023-01-18 17:34:15 +0100

    feat: add modal

--- src/assets/js/components/script.js
+++ src/assets/js/components/script.js
@@ -2,6 +2,10 @@ const burger = document.querySelector(".burger");
 const burgerMenu = document.querySelector(".burger__wrapper");
 const burgerClose = document.querySelector(".burger__close");
 const overlay = document.querySelector(".overlay");
+const headerBtn = document.querySelector(".header__btn");
+const modal = document.querySelector(".modal-form");
+const burgerBtn = document.querySelector(".burger__btn");
+const modalClose = document.querySelector(".modal-form__close");
 
 burger.addEventListener('click', function() {
   burgerMenu.classList.toggle("burger__wrapper_active");
@@ -15,5 +19,22 @@ burgerClose.addEventListener('click', function() {
 
 overlay.addEventListener('click', function() {
   burgerMenu.classList.remove("burger__wrapper_active");
+  overlay.classList.remove("overlay_active");
+  modal.classList.remove("modal-active");
+});
+
+headerBtn.addEventListener('click', function() {
+  modal.classList.toggle("modal-active");
+  overlay.classList.toggle("overlay_active");
+});
+
+burgerBtn.addEventListener('click', function() {
+  modal.classList.toggle("modal-active");
+  // overlay.classList.toggle("overlay_active");
+  burgerMenu.classList.remove("burger__wrapper_active");
+});
+
+modalClose.addEventListener('click', function() {
+  modal.classList.remove("modal-active");
   overlay.classList.remove("overlay_active");
 });
